64d84625435563be7c375ad0261506ec2b75cfa8
Adam <adammurdocknh@gmail.com>
2020-11-13 23:39:40 -0500

    Updating, and cleaning source code for final submission.

--- src/net.py
+++ src/net.py
@@ -1,13 +1,14 @@
 import numpy as np
-from torch._C import dtype
 
-weights = np.array([0.56774643,  0.47112446, -1.04678128,  0.95022474,  1.09802062,
-                    -0.15844113, -0.03392968, -0.01863788,  0.41180123, -0.54731532,
-                    -0.22394692, -1.69846885, -0.01616816], dtype=np.float64)
+weights = np.array([ 2.72945449e-02, -4.56520008e-01, -3.05190125e+00,  3.02470025e+00,
+        5.88618485e-01, -8.24037571e-02, -5.60063530e-01,  6.10677734e-01,
+        8.45962219e-01, -1.89185232e-01,  4.97685142e-03, -1.11223591e+01,
+        3.28843658e-01], dtype=np.float64)
 
-# alpha = .00000001
-alpha = .0000001
+# If you want to generate random values
+# weights = np.random.rand(13,)
 
+alpha = .000001
 
 mainSheet = np.load('./src/main.npy')
 
@@ -23,42 +24,21 @@ win_ratio_sheet = mainSheet[:,15:]
 print(np.size(weights))
 # 1000 235.37
 
-cycles = 1
-
-
-# def Average(lst):
-#     return sum(lst) / len(lst)
-
+cycles = 1000
 
 avgWin = np.mean(win_ratio_sheet)
-# avgWin = np.empty(1,dtype='float64')
-avgError = np.array(1, dtype='float64')
-finalPredition = []
 
-# for number of cycles:
-#   for i in number of rows in input:
-#       for cell in row
-#           row weighted with np.multiply
-#           pred = row summed with np.sum()
-#           error = actual winrate - prediction ** 2
-#           delta = prediction - actual winrate
-#           new weight = old weights - (alpha * (input * delta))
-#           weights = weights - (alpha * input)
-#           print('Error:' + str(error))
-#           print('Prediction:' + str(prediction))
-
-
+avg_error = np.zeros(1, dtype='float64')
+avg_prediction = np.zeros(1,dtype='float64')
+avg_delta = np.zeros(1,dtype='float64')
 
 for i in range(cycles):
     j = 0
-    # print(weights)
+        
     for row in players_sheet:
         
         index = j
         prediction = row.dot(weights)
-        # print(prediction)
-        # print(j)
-        np.append(weighted_sheet,prediction)
         
         error = (win_ratio_sheet[j] - prediction) ** 2
         
@@ -66,33 +46,25 @@ for i in range(cycles):
         weights = weights - (alpha * (row * delta))
         
         # print("Error:" + str(error) + " Prediction:" + str(prediction))
-        np.append(avgError,error)
-        # np.append(avgWin,win_ratio_sheet[j])
-        # predictions.append(prediction)
-        # avgError.append(error)
-        # avgWin.append(win_ratio_sheet[j])
-        j += 1
+        avg_error = np.append(avg_error,error)
+        avg_prediction = np.append(avg_prediction,prediction)
+        avg_delta = np.append(avg_delta,delta)
         
+        j += 1
+    
+
     finalPredition = prediction
-    if i % (cycles/10) == 0:
+    if i % (cycles/100) == 0:
         
         print('Cycle i:', i)
         print('Target:', avgWin)
-        print('Prediction:', prediction)
-        print('error', error)
-        print('Delta:', delta)
-        print(repr(weights))
+        print('Prediction:', np.mean(avg_prediction))
+        print('Avg Error', np.sqrt(np.mean(avg_error)))
+        print('Delta:', np.mean(avg_delta))
+        
 
-# print('Final error %:', ((Average(avgError)/Average(avgWin))*100))
-# print('avg error:',np.mean(avgError))
-print('avg win:', avgWin, '%')
-print('final prediction:',finalPredition,'%')
+        avg_error = np.zeros(1, dtype='float64')
+        avg_prediction = np.zeros(1,dtype='float64')
+        avg_delta = np.zeros(1,dtype='float64')
+        
 print(repr(weights))
-
-
-
-                
-
-# for i in range(cycles):
-#     prediction = input.dot(weights)
-    # error = (goal_prediction - prediction)
